e4a817b79003f2cd9aa9c9b2c8e2cd7b3ed831ee
Firmino Changani <firmino.changani@jungle.ai>
2019-07-13 20:31:37 +0100

    Add card component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <gf-card is-rounded>
+      <!-- Header -->
+      <div class="gf-card__header">
+        <h1 class="gf-card__header__value">103,905.99</h1>
+        <span class="gf-card__header__date">{{ new Date() }}</span>
+      </div>
+
+      <!-- Tab nav -->
+
+      <!-- Line chart -->
+    </gf-card>
+  </div>
 </template>
 
 <script>
---
+++ src/components/gf-card.vue
@@ -0,0 +1,40 @@
+<template>
+  <article :class="cssClasses">
+    <slot></slot>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "GoogleFinanceCard",
+  data() {
+    return {
+      cssClasses: {
+        "gf-card": true,
+        "is-rounded": false
+      }
+    };
+  },
+  props: {
+    "is-rounded": {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted() {
+    this.cssClasses["is-rounded"] = this.$props.isRounded;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$card-border-color: #ddd;
+.gf-card {
+  padding: 15px;
+  border: 1px solid $card-border-color;
+
+  &.is-rounded {
+    border-radius: 3px;
+  }
+}
+</style>
