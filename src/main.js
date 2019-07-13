85249de7848bc5f60f39702fdcce6edce5b648c3
Firmino Changani <firmino.changani@jungle.ai>
2019-07-13 20:31:03 +0100

    Add fonts and global styles

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+// Global styles
+import "@/assets/styles/base.scss";
+// Global components
+import gfCard from "@/components/gf-card";
 
 Vue.config.productionTip = false;
+Vue.component("gf-card", gfCard);
 
 new Vue({
   render: h => h(App)
