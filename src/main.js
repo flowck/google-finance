31ed5a22d4ad9c26e61f50c05a7940439ce2b97f
Firmino Changani <firmino.changani@jungle.ai>
2019-07-14 08:38:18 +0100

    Register linechart component

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import App from "./App.vue";
 import "@/assets/styles/base.scss";
 // Global components
 import gfCard from "@/components/gf-card";
+import gfLineChart from "@/components/gf-linechart";
 
 Vue.config.productionTip = false;
+// Register components
 Vue.component("gf-card", gfCard);
+Vue.component("gf-linechart", gfLineChart);
 
 new Vue({
   render: h => h(App)
