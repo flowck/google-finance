0aec15948f8546be33f786f09c1a7e6fe748622c
Firmino Changani <firmino.changani@jungle.ai>
2019-07-14 09:58:02 +0100

    Add nav component

---
+++ src/components/gf-nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="gf-nav">
+    <ul class="flex">
+      <li>
+        All
+      </li>
+      <li>Finance</li>
+      <li>News</li>
+      <li>Images</li>
+      <li>Videos</li>
+      <li>More</li>
+      <li>Settings</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "GFNav",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.gf-nav {
+  ul {
+    height: 30px;
+    // border: 1px solid red;
+    list-style: none;
+    margin-left: 11px;
+
+    li {
+      padding: 0 10px;
+      font-size: 12.5px;
+    }
+  }
+}
+</style>
