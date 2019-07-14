f4a3d03f787b88346c87c5f619b1a32fb14603d5
Firmino Changani <firmino.changani@jungle.ai>
2019-07-14 17:25:00 +0100

    Update props

--- src/components/gf-news.vue
+++ src/components/gf-news.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="gf-news">
-    <gf-card is-rounded class="flex flex--has-space-between">
+    <gf-card is-rounded :radius="radius" class="flex flex--has-space-between">
       <div>
         <div class="gf-news__title">
-          <h1>{{ title }}</h1>
+          <a href="#">
+            <h1>{{ title }}</h1>
+          </a>
         </div>
-        <div class="gf-news__meta">
-          {{ date | formatDate }}
-        </div>
+        <div class="gf-news__meta">{{ source }} · {{ date | formatDate }}</div>
       </div>
-      <div class="gf-news__thumbnail">
+      <div class="gf-news__thumbnail flex flex--centered-y">
         <img :src="thumbnail" :alt="title" />
       </div>
     </gf-card>
@@ -33,8 +33,14 @@ export default {
       required: true
     },
     date: {
-      type: Date,
+      type: String,
       required: true
+    },
+    radius: {
+      type: String
+    },
+    source: {
+      type: String
     }
   },
   filters: {
@@ -48,13 +54,31 @@ export default {
 <style lang="scss" scoped>
 .gf-news {
   .gf-news__meta {
-    font-size: 12px;
+    font-size: 13px;
+    margin-top: 10px;
+  }
+
+  .gf-news__title {
+    h1 {
+      font-weight: normal;
+    }
+
+    a {
+      color: #1a0dab;
+      font-size: 16px;
+      text-decoration: none;
+    }
   }
 
   .gf-news__thumbnail {
     img {
       width: 150px;
+      border-radius: 3px;
     }
   }
+}
+
+.gf-news:hover a {
+  text-decoration: underline;
 }
 </style>
